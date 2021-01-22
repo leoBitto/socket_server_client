e2f654de43f52fb37046e16287120efc978c4da5
leoBitto <leonardobitto@gmail.com>
2021-01-22 10:02:21 +0100

    tolto print

--- server.py
+++ server.py
@@ -4,15 +4,12 @@ import socket
 
 s = socket.socket()
 host = socket.gethostname()
-print(s)
-print(host)
 port = 12345
 s.bind((host, port))
 
 s.listen(5)
 while True:
     c, addr = s.accept()
-    print(c)
-    print(addr)
+    print(f'conection with {addr}')
     c.send('thank you for connecting')
     c.close()
